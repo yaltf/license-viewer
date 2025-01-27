d5e948e94cf4ef820b38262f809bf23008ed09a4
Sankalp <mail@sghatpande.eu>
2025-01-27 16:05:29 +0100

    Documentation (#2)
    * [Add] License header
    
    * Update documentation
    
    * [Update] Readme
    
    add the webaddress, YALTF reference
    
    ---------
    
    Co-authored-by: Doc <meow@Blackbox.local>
    Co-authored-by: Ziya Genc <ziya.genc@datasecure.ai>

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-only.
+// SPDX-FileCopyrightText: 2025 International Committee of the Red Cross.
+//
+// This software is licensed under GPL v3.0 license.
+// See LICENSE file at the root of the project.
+
 package main
 
 import (
